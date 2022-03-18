c64a14b075a26ec035499677595733e60a17744c
LuanNg <thithaicm1@gmail.com>
2022-03-18 16:01:38 +0700

    upload code

---
+++ src/app/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="nav">
+      <div className="nav-logo-wrap">
+        <img
+          src="https://www.w3schools.com/css/img_trans.gif"
+          className="nav--logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
